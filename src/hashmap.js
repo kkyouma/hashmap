cf10faf865e480cd4b70dbb1d7eb33985ed744b4
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-01-30 00:05:34 -0300

    add get and has methods

--- src/hashmap.js
+++ src/hashmap.js
@@ -4,12 +4,13 @@ class HashMap {
   }
 
   hash(key) {
+    const modulo = 16; // should be change when the storage is full
     const characters = Array.from(key);
 
     let value = 0;
     characters.forEach(e => {
       const eAscii = e.charCodeAt(0);
-      value += parseInt(eAscii) % 16;
+      value += parseInt(eAscii) % modulo;
       return value;
     });
 
@@ -24,6 +25,21 @@ class HashMap {
     }
 
     return (this._storage[hashValue] = { key, value });
+  }
+
+  get(key) {
+    const hashValue = this.hash(key);
+    if (this._storage[hashValue]) {
+      return this._storage[hashValue].value;
+    } else {
+      return null;
+    }
+  }
+
+  has(key) {
+    const hashValue = this.hash(key);
+
+    return !!this._storage[hashValue];
   }
 }
 
